a14430a5024e951a78752843c6898f8c55f48adb
Artur Karpovich <onelifeitsme@gmail.com>
2024-04-24 19:07:05 +0300

    token by dotenv

--- steam_api.py
+++ steam_api.py
@@ -1,7 +1,5 @@
 import requests
-
-TOKEN = '3026AC4D563D119B5D945F2624A36D53'
-
+from loading import API_KEY
 
 class Player:
 
@@ -9,29 +7,28 @@ class Player:
         self.steam_id = steam_id
 
     def get_match_info(self, match_id):
-        url = f'https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?key={TOKEN}&match_id={match_id}'
+        url = f'https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?key={API_KEY}&match_id={match_id}'
         response = requests.get(url)
         return response
 
     def get_player_info(self, steam_id):
-        url = f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={TOKEN}&steamids={steam_id}"
+        url = f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={API_KEY}&steamids={steam_id}"
         response = requests.get(url)
         player_data = response.json()
         return player_data["response"]["players"][0]
 
 
     def get_match_history(self, num_matches=100, start_match_id=None):
-        url = f"https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key={TOKEN}&account_id={self.steam_id}&matches_requested={num_matches}"
+        url = f"https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key={API_KEY}&account_id={self.steam_id}&matches_requested={num_matches}"
         if start_match_id:
             url += f"&start_at_match_id={start_match_id}"
         response = requests.get(url)
         match_history = response.json()
         return match_history["result"]["matches"]
 
-    def get_all_matches(self):
-        a = int(2000/100)
+    def get_all_matches(self, matches_amount):
         self.all_matches = []
-        for i in range(a):
+        while len(self.all_matches) < matches_amount:
             if not hasattr(self, 'last_match_id'):
                 matches = self.get_match_history()
             else:
@@ -46,7 +43,7 @@ class Player:
 
 
 player = Player(steam_id='76561198081994070')
-player.get_all_matches()
+player.get_all_matches(matches_amount=2746)
 ...
 
 
